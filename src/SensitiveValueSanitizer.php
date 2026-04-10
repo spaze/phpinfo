f730294d9e0b684f1a9aa3c241ff0bb505cea001
Michal Špaček <mail@michalspacek.cz>
2026-04-10 19:20:03 +0200

    Call getSessionId() just once

--- src/SensitiveValueSanitizer.php
+++ src/SensitiveValueSanitizer.php
@@ -26,9 +26,12 @@ final class SensitiveValueSanitizer
 	public function sanitize(string $info): string
 	{
 		$sanitize = [];
-		if ($this->sanitizeSessionId && $this->getSessionId() !== null) {
-			$sanitize[$this->getSessionId()] = $this->sanitizeWith;
-			$sanitize[urlencode($this->getSessionId())] = $this->sanitizeWith;
+		if ($this->sanitizeSessionId) {
+			$sessionId = $this->getSessionId();
+			if ($sessionId !== null) {
+				$sanitize[$sessionId] = $this->sanitizeWith;
+				$sanitize[urlencode($sessionId)] = $this->sanitizeWith;
+			}
 		}
 		return strtr($info, $this->sanitize + $sanitize);
 	}
